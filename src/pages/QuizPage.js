d1a63cba603a6389e4d984adf0800584f12e11fd
juyonLee00 <ljy012900@gmail.com>
2021-10-12 20:17:51 +0900

    ui 수정 및 페이지 추가

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -1,39 +1,37 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import ButtonComponent from '../components/Buttons';
-import contents from '../contents/questions';
-import Parser from 'html-react-parser';
-import { Redirect } from 'react-router-dom'
+import React, { useState } from "react";
+import styled from "styled-components";
+import ButtonComponent from "../components/Buttons";
+import contents from "../contents/questions";
+import Parser from "html-react-parser";
+import { Redirect } from "react-router-dom";
 
 const Wrapper = styled.div`
-  display: ${props => props.isShow === true ? 'flex' : 'none'};
-
-  background-color:white;
-  flex-direction:column;
-  align-items:center;
+  display: ${(props) => (props.isShow === true ? "flex" : "none")};
+  background-color: white;
+  flex-direction: column;
+  align-items: center;
   text-align: center;
-`
+`;
 
 const Text = styled.div`
   position: absolute;
   margin-top: 100px;
   margin-bottom: 200px;
-  font-family: 'GmarketSansMedium';
+  font-family: "GmarketSansMedium";
   font-style: normal;
   font-weight: 500;
   font-size: 18px;
   line-height: 132%;
   text-align: center;
-  color: #2D2D2D;
-`
+  color: #2d2d2d;
+`;
 const ButtonWrap = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 19rem;
-`
+`;
 
 function QuizPage({ isShow }) {
-
   const [questionNum, setQuestionNum] = useState(0);
   const [linkTo, setLinkTo] = useState("");
   const linkResult = "/result/";
@@ -44,10 +42,10 @@ function QuizPage({ isShow }) {
   // const [finalType, setFinalType] = useState(0);
 
   const onConditionChange = (key) => {
-
     let record = contents[questionNum].answers[key].score;
     /* 결과 계산 알고리즘 */
-    if (questionNum === 0 || questionNum === 7 || questionNum === 8) { // EI
+    if (questionNum === 0 || questionNum === 7 || questionNum === 8) {
+      // EI
       setTypeEI(typeEI + record);
     } else if (questionNum === 2 || questionNum === 3 || questionNum === 9) {
       setTypeSN(typeSN + record);
@@ -55,53 +53,58 @@ function QuizPage({ isShow }) {
       setTypeTF(typeTF + record);
     } else if (questionNum === 1 || questionNum === 4 || questionNum === 5) {
       setTypeJP(typeJP + record);
-    } else if (questionNum === 11) { // 마지막 문제일때
+    } else if (questionNum === 11) {
+      // 마지막 문제일때
       setTypeTF(typeTF + record);
 
       let result = 0; //result 값 초기화
 
       if (typeEI >= 2) {
         result = result + 8;
-      };
+      }
       if (typeSN >= 2) {
         result = result + 4;
-      };
+      }
       if (typeTF >= 2) {
         result = result + 2;
-      };
+      }
       if (typeJP >= 2) {
         result = result + 1;
-      };
+      }
 
-    let num = result;
-    // setFinalType(num);
-    setLinkTo(linkResult + num);
-    // setQuestionNum(16);
+      let num = result;
+      // setFinalType(num);
+      setLinkTo(linkResult + num);
+      // setQuestionNum(16);
     }
     setQuestionNum(questionNum + 1); /* 문제 번호 하나씩 증가 */
-  }
+  };
 
-  if (questionNum < 12) { /* 1~11 문제 버튼 누르면 */
+  if (questionNum < 12) {
+    /* 1~11 문제 버튼 누르면 */
     return (
       <>
         <Wrapper isShow={isShow}>
-            <Text>{Parser(contents[questionNum].question)}</Text> {/* 순서대로 질문 보여주기 */}
-            <ButtonWrap> 
+          <Text>{Parser(contents[questionNum].question)}</Text>{" "}
+          {/* 순서대로 질문 보여주기 */}
+          <ButtonWrap>
             {contents[questionNum].answers.map((answer, i) => (
-                <ButtonComponent
-                  key={'answer' + i}
-                  idx={i}
-                  text={Parser(answer.text)}
-                  onclick={onConditionChange}
-                />
-              ))}
-            </ButtonWrap>
+              <ButtonComponent
+                key={"answer" + i}
+                idx={i}
+                text={Parser(answer.text)}
+                onclick={onConditionChange}
+              />
+            ))}
+          </ButtonWrap>
         </Wrapper>
-      </>);
-  } else if (questionNum === 12) { /* 마지막 문제 버튼 누르면 결과페이지로 이동 */
+      </>
+    );
+  } else if (questionNum === 12) {
+    /* 마지막 문제 버튼 누르면 결과페이지로 이동 */
     return (
       <>
-          <Redirect to={linkTo}></Redirect>
+        <Redirect to={linkTo}></Redirect>
       </>
     );
   }
